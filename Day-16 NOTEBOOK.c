d2dfd0708561b485e38995810efa0b8a3720c4ea
Ram-bhardwaj12 <rambhardwaj1204@gmail.com>
2024-10-13 20:18:11 +0530

    Add files via upload

---
+++ Day-16 NOTEBOOK.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int T, N;
+    scanf("%d", &T);
+    for (int i = 0; i < T; i++) {
+        scanf("%d", &N); 
+        printf("%d\n", N * 10); 
+    }
+
+    return 0;
+}
