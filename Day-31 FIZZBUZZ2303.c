35c119299c1044cdfb4b2235c40f72c5e4391145
Ram-bhardwaj12 <rambhardwaj1204@gmail.com>
2024-11-02 10:20:09 +0530

    Add files via upload

---
+++ Day-31 FIZZBUZZ2303.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int T;  
+    scanf("%d", &T);
+
+    while (T--) {
+        int N;
+        scanf("%d", &N);
+
+        int choices = N * (N - 1);
+        printf("%d\n", choices);
+    }
+
+    return 0;
+}
